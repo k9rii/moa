f62893abd23fe14a5e48c44fbb21817c3e4baa40
김규리 <cherrychel@naver.com>
2026-01-06 21:13:29 +0900

    로고 추가 및 색 변경

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -14,18 +14,25 @@ export function Login() {
 
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
-      <div className="w-full max-w-sm rounded-2xl bg-white p-8 shadow-xl space-y-6">
-        <h1 className="text-2xl font-semibold text-center">Login</h1>
+      <div
+        className="w-full max-w-sm rounded-2xl p-8 space-y-6
+              bg-[#FEFAFB]
+              border border-[#F2C2CF]
+              shadow-[0_8px_24px_rgba(0,0,0,0.08)]"
+      >
+        <h1 className="text-xl font-semibold text-center text-[#F26B83]">
+          Login
+        </h1>
 
         <input
-          className="w-full border p-3 rounded-lg"
+          className="w-full border border-gray-200 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F29BAB]"
           placeholder="moa@gmail.com"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
-          className="w-full border p-3 rounded-lg"
+          className="w-full border border-gray-200 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F29BAB]"
           type="password"
           placeholder="password"
           value={password}
@@ -34,14 +41,17 @@ export function Login() {
 
         <button
           onClick={handleLogin}
-          className="w-full bg-black text-white py-3 rounded-lg"
+          className="w-full bg-[#F26B83] text-[#FEFAFB] py-3 rounded-lg hover:bg-[#F29BAB] transition"
         >
           Login
         </button>
 
         <p className="text-sm text-center text-gray-500">
           Not a membet yet?{" "}
-          <Link to="/signup" className="text-violet-500 font-medium">
+          <Link
+            to="/signup"
+            className="text-[#F26B83] font-medium hover:underline"
+          >
             Sign Up
           </Link>
         </p>
